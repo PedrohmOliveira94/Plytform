06e6dd6fe4bf9582f8039389db6682087a4c0c8f
Pedro Henrique Mendes <phmoliveira94@gmail.com>
2022-04-01 18:48:07 -0300

    Adding test map, crude player collision and gravity

---
+++ settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'                            ',
+'                            ',
+'                 X          ',
+'        P                   ',
+'        X    X     X        ',
+'                            ',
+'     X                      ',
+'         X                  ',
+'                            ',
+'                            ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ',]
+
+tile_size = 64
+screen_width = 1200
+screen_height = tile_size * len(level_map)
