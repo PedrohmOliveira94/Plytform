7294e80256b7d3696b3e461503535340cebe2f8c
Pedro Henrique Mendes <phmoliveira94@gmail.com>
2022-04-13 14:24:03 -0300

    Adding particle effects to player movement

--- level.py
+++ level.py
@@ -2,6 +2,7 @@ import pygame
 from tiles import Tile
 from settings import *
 from player import Player
+from particles import ParticleEffect
 
 
 class Level:
@@ -12,6 +13,34 @@ class Level:
         self.setup_level(level_data)
         self.world_shift = 0
         self.current_x = 0
+
+        #dust
+        self.dust_sprite = pygame.sprite.GroupSingle()
+        self.player_on_ground = False
+
+    def create_jump_particle(self, pos):
+        if self.player.sprite.facing_right:
+            pos -= pygame.math.Vector2(10, 5)
+        else:
+            pos += pygame.math.Vector2(10, -5)
+        jump_particle_sprite = ParticleEffect(pos, 'jump')
+        self.dust_sprite.add(jump_particle_sprite)
+
+    def get_player_on_ground(self):
+        if self.player.sprite.on_ground:
+            self.player_on_ground = True
+        else:
+            self.player_on_ground = False
+
+    def create_landing_dust(self):
+        if not self.player_on_ground and self.player.sprite.on_ground and not self.dust_sprite.sprites():
+            if self.player.sprite.facing_right:
+                offset = pygame.math.Vector2(10, 15)
+            else:
+                offset = pygame.math.Vector2(-10, 15)
+
+            fall_dust_particle = ParticleEffect(self.player.sprite.rect.midbottom - offset, 'land')
+            self.dust_sprite.add(fall_dust_particle)
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
@@ -26,7 +55,7 @@ class Level:
                     tile = Tile((x, y), tile_size)
                     self.tiles.add(tile)
                 if cell == 'P':
-                    player_sprite = Player((x, y))
+                    player_sprite = Player((x, y), self.display_surface, self.create_jump_particle)
                     self.player.add(player_sprite)
 
     def scroll_x(self):
@@ -85,6 +114,10 @@ class Level:
 
 
     def run(self):
+        # dust particles
+        self.dust_sprite.update(self.world_shift)
+        self.dust_sprite.draw(self.display_surface)
+
         #level
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
@@ -92,6 +125,9 @@ class Level:
 
         #player
         self.player.update()
-        self.player.draw(self.display_surface)
         self.horizontal_collision()
+        self.get_player_on_ground()
         self.vertical_collision()
+        self.create_landing_dust()
+        self.player.draw(self.display_surface)
+
